d5711d9979de6f459d34e6f154aa93263a807e79
MedSveta <sveta1978medved@gmail.com>
2025-12-12 12:01:50 +0200

    Login Positive Test

---
+++ src/test/java/mobile_tests/LoginTests.java
@@ -0,0 +1,33 @@
+package mobile_tests;
+
+import config.AppiumConfig;
+import dto.RegistrationBodyDto;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import screens.LoginScreen;
+import screens.SearchScreen;
+import screens.SplashScreen;
+
+public class LoginTests extends AppiumConfig {
+    @BeforeMethod
+    public void openLoginScreen(){
+        new SplashScreen(driver).goToSearchScreen(7);
+        SearchScreen searchScreen = new SearchScreen(driver);
+        searchScreen.clickBtnDots();
+        searchScreen.clickBtnLogin();
+    }
+
+    @Test
+    public void loginPositiveTest(){
+        RegistrationBodyDto user = RegistrationBodyDto.builder()
+                .username("sima_simonova370@gmail.com")
+                .password("BSas124!")
+                .build();
+        LoginScreen loginScreen = new LoginScreen(driver);
+        loginScreen.typeLoginForm(user);
+        loginScreen.clickBtnYalla();
+        Assert.assertTrue(new SearchScreen(driver)
+                .validatePopUpMessageSuccess("Login success!"));
+    }
+}
